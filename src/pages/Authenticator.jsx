26b9a426fa16417ceb4d299ea794efec0f314871
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-10 18:20:26 -0400

    Update Authenticator.jsx

--- src/pages/Authenticator.jsx
+++ src/pages/Authenticator.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Login from '../components/Login';
+import Register from '../components/Register';
 
 export default function Authenticator() {
   return (
-    <div>Authenticator</div>
-  )
+    <div className='auth'>
+      <Login/>
+      <Register/>
+    </div>
+  );
 }
