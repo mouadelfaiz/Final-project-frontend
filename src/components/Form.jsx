141314be0b216812b3a35b7ef93a531b77d8acdf
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-10 18:20:12 -0400

    Create Form.jsx

---
+++ src/components/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Form({ username, setUsername, password, setPassword, label, onSubmit }) {
+  return (
+    <div className='auth-container'>
+      <form onSubmit={onSubmit}>
+        <h2>{label}</h2>
+        <div className='form-group'>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </div>
+        <button type='submit'>{label}</button>
+      </form>
+    </div>
+  );
+}
