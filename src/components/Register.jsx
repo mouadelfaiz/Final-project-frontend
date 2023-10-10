60b6af18aeebaf45acb95eee09a140b006eb5546
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-10 18:20:23 -0400

    Create Register.jsx

---
+++ src/components/Register.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import axios from "axios";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {username,password})
+      alert("Registration Completed! Now login")
+    } catch (error) {
+console.error(error)
+    }
+  };
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Sign Up"
+      onSubmit={onSubmit}
+    />
+  );
+}
