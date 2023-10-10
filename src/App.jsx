6a1afefddfc162a6cdacff4120cabb0c930c8c57
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-10 18:20:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Route, Router, Routes } from 'react-router'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Authenticator from './pages/Authenticator'
+import CreateProject from './pages/CreateProject'
+import Projects from './pages/Projects'
+import NavBar from './components/NavBar'
+
 
 
 function App() {
-
-
   return (
    <div>
     <Router>
+      <NavBar/>
       <Routes>
-        <Route path='/' element={}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Authenticator/>}/>
+        <Route path='/create-project' element={<CreateProject/>}/>
+        <Route path='/projects' element={<Projects/>}/>
       </Routes>
     </Router>
    </div>
