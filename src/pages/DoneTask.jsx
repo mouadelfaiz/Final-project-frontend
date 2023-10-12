7b7d3b07dc58d4fff512f43a69b798f48c71d084
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-12 07:45:52 -0400

    Create DoneTask.jsx

---
+++ src/pages/DoneTask.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import useGetUserID from "../hooks/useGetUserID";
+import axios from "axios";
+
+export default function DoneTask() {
+const [doneProjects, setDoneProjects] = useState([]);
+const userID = useGetUserID();
+
+useEffect(() => {
+  const fetchDoneProject = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/projects/doneprojects/${userID}`
+      );
+      setDoneProjects(response.data.doneProjects);
+    } catch (err) {
+      console.error("Error fetching done projects, err");
+    }
+  };
+
+  fetchDoneProject();
+}, []);
+return (
+  <div>
+    <h1>Saved projects</h1>
+    <ul>
+      {doneProjects.map((project) => (
+        <li key={project._id}>
+          <div>
+            <h2>{project.name}</h2>
+          </div>
+          <p>{project.description}</p>
+          <img src={project.imageUrl} alt={project.name} />
+          <p>Cooking Time: {project.cookingTime} minutes</p>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+}
